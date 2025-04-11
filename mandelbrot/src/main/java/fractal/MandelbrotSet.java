62a03e22756129f7231cf451f6087b588bf59ce4
Disha <disha.wind@gmail.com>
2025-04-11 11:09:00 +0300

    update fractal

--- mandelbrot/src/main/java/fractal/MandelbrotSet.java
+++ mandelbrot/src/main/java/fractal/MandelbrotSet.java
@@ -4,8 +4,7 @@ import main.java.complex.ComplexNumber;
 
 public class MandelbrotSet {
     private int maxIterations = 200;
-    public final double R2 = 4.0;
-    
+
     public int getMaxIterations() {
         return maxIterations;
     }
@@ -20,13 +19,17 @@ public class MandelbrotSet {
         this.maxIterations = maxIterations;
     }
     
-    public boolean isInSet(ComplexNumber num){
+    public int getCountOfPassIteration(ComplexNumber num){
         int i = 0;
         ComplexNumber z = new ComplexNumber();
-        while (i++ < maxIterations && z.abs2() < R2) {
+        while (i++ < maxIterations && z.abs2() < 4.0) {
             z.times(z);
             z.plus(num);
         }
-        return i > maxIterations;
+        return i - 1;
+    }
+    
+    public boolean isInSet(ComplexNumber num){
+        return getCountOfPassIteration(num) == maxIterations;
     }
 }
