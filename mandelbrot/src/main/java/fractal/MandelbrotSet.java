c4859a5c1adf2f112ae37b6c680956cfe973e1ed
Disha <disha.wind@gmail.com>
2025-04-11 10:54:45 +0300

    add tests for fractal class

--- mandelbrot/src/main/java/fractal/MandelbrotSet.java
+++ mandelbrot/src/main/java/fractal/MandelbrotSet.java
@@ -1,17 +1,31 @@
-﻿package main.java.fractal;
+package main.java.fractal;
 
 import main.java.complex.ComplexNumber;
 
 public class MandelbrotSet {
-    private int maxIterations = 100;
-    private final double R2 = 4.0;
+    private int maxIterations = 200;
+    public final double R2 = 4.0;
+    
+    public int getMaxIterations() {
+        return maxIterations;
+    }
+
+    public void setMaxIterations(int maxIterations) {
+        if (maxIterations < 0) {
+            throw new IllegalArgumentException("maxIterations cannot be negative");
+        }
+        if (maxIterations < 10) {
+            throw new IllegalArgumentException("maxIterations too small (must be at least 10)");
+        }
+        this.maxIterations = maxIterations;
+    }
     
     public boolean isInSet(ComplexNumber num){
         int i = 0;
         ComplexNumber z = new ComplexNumber();
         while (i++ < maxIterations && z.abs2() < R2) {
             z.times(z);
-            z.plus(z);
+            z.plus(num);
         }
         return i > maxIterations;
     }
---
+++ mandelbrot/test/test/java/fractal/MandelbrotSetTest.java
@@ -0,0 +1,54 @@
+package test.java.fractal;
+
+import main.java.complex.ComplexNumber;
+import main.java.fractal.MandelbrotSet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MandelbrotSetTest {
+    
+    @Test
+    public void isInSet(){
+        var f = new MandelbrotSet();
+
+        var inSet = new ArrayList<>(Arrays.asList(
+                new ComplexNumber(0, 0),
+                new ComplexNumber(-1, 0),
+                new ComplexNumber(-0.75, 0),
+                new ComplexNumber(0.25, 0.5),
+                new ComplexNumber(-0.1015, 0.633)
+        ));
+
+        var notInSet = new ArrayList<>(Arrays.asList(
+                new ComplexNumber(2, 0),
+                new ComplexNumber(-2, 0),
+                new ComplexNumber(1, 1),
+                new ComplexNumber(-1.5, 1.5),
+                new ComplexNumber(0.5, 0.75)
+        ));
+        
+        for (ComplexNumber c : inSet) {
+            Assertions.assertTrue(f.isInSet(c));
+        }
+        
+        for (ComplexNumber c : notInSet) {
+            Assertions.assertFalse(f.isInSet(c));
+        }
+    }
+    
+    @Test
+    public void maxIterations(){
+        var f = new MandelbrotSet();
+        f.setMaxIterations(1000);
+        
+        Assertions.assertEquals(1000, f.getMaxIterations());
+        
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {f.setMaxIterations(0);});
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {f.setMaxIterations(-1);});
+        
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {f.setMaxIterations(5);});
+    }
+}
