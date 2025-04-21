7a05076de79554d560ec324adadc11f658dd13e1
Disha <disha.wind@gmail.com>
2025-04-21 16:01:52 +0300

    add size to painter

--- src/com/disha/app/fractal/painting/AbstractPainter.java
+++ src/com/disha/app/fractal/painting/AbstractPainter.java
@@ -2,10 +2,28 @@ package com.disha.app.fractal.painting;
 
 import com.disha.converter.Converter;
 
+import java.awt.*;
+
 public abstract class AbstractPainter implements Painter {
+    protected Dimension size;
     protected Converter converter;
     
     public AbstractPainter(Converter converter) {
         this.converter = converter;
+    }
+
+    @Override
+    public Dimension getSize() {
+        return new Dimension(size);
+    }
+
+    @Override
+    public void setSize(Dimension size) {
+        this.size = size;
+    }
+
+    @Override
+    public void setSize(int width, int height) {
+        this.size = new Dimension(width, height);
     }
 }
--- src/com/disha/app/fractal/painting/Painter.java
+++ src/com/disha/app/fractal/painting/Painter.java
@@ -3,5 +3,8 @@ package com.disha.app.fractal.painting;
 import java.awt.*;
 
 public interface Painter {
+    Dimension getSize();
+    void setSize(Dimension size);
+    void setSize(int width, int height);
     void paint(Graphics graphics);
 }
